102529fc5394f3f3676548ab9b5b6c3a1fc3a49e
sowmyakatta <sowmya320@gmail.com>
2020-11-08 21:30:59 +0530

    g

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, jsonify
 from flask_cors import CORS,cross_origin
 from predict import MyPredictor
 from my_utils.utils import decodeImage
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -27,6 +28,7 @@ def predictRoute():
     return render_template("Predicted result.html", result=result)
 
 clApp = ClientApp("test2.jpg")
+port = int(os.environ.get("PORT", 5000))
 
 if __name__=="__main__":
-    app.run(port=8000, debug=True)
+    app.run(port=port, debug=True)
